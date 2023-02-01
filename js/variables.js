58fd4dec3a3363f5d386cfe0b10138866f0436df
Salas <hernan.salas@comafi.com.ar>
2023-02-01 09:31:17 -0300

    cosas de js

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+//alert('hola mundo desde js');
+/* variables: se puede cambiar*/
+let edad = 36;
+console.log('edad: '+edad);
+edad = 45;
+console.log('edad: '+edad);
+
+/**constante no se puede cambiar */
+const dni = '24835403';
+alert("dni: "+dni);
+//dni = '343';
